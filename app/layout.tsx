de0cd714e1e59721b23e0739485cbf8e7efdd0ec
Anjal Patel <patel.anjal567@gmail.com>
2024-10-03 21:21:42 +0530

    eleven

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const inter = Inter ({ subsets: ["latin"]});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
